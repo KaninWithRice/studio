540f70bb2448c8d553203f5c8417ae536b4bbb7f
Mishal Noroña <mishalnorona18@gmail.com>
2025-06-07 10:42:09 +0000

    use this instead for data base
    const firebaseConfig = {
      apiKey: "AIza

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,16 @@
+
 // Ensure you have initialized Firebase in your project and set up Firestore.
 
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID",
+  apiKey: "AIzaSyByhVfI0_PWMygt5y1j8LBMY7aVE6JvIjg",
+  authDomain: "noodle-hub.firebaseapp.com",
+  projectId: "noodle-hub",
+  storageBucket: "noodle-hub.firebasestorage.app",
+  messagingSenderId: "230957040823",
+  appId: "1:230957040823:web:4ecbc107c4711525c37932"
 };
 
 // Initialize Firebase
